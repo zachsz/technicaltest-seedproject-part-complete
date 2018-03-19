b731039fa8a68094fa129914b5925292861637b2
Mathew Hawley <Mathew.Hawley@decision.tech>
2018-03-19 09:17:31 +0000

    remove unneeded <div>

--- decision-tech-react-seed/src/components/deal-filter/deal-filter.jsx
+++ decision-tech-react-seed/src/components/deal-filter/deal-filter.jsx
@@ -5,37 +5,35 @@ import './deal-filter.scss';
 class DealFilter extends React.Component {
   render() {
     return (
-      <div className="deal-filter-menu">
-        <ul>
-          <li>
-            <input
-              id="filter-broadband"
-              type="checkbox"
-              onChange={this.props.handleChange}
-              value="broadband"
-            />
-            <label htmlFor="filter-broadband">Broadband</label>
-          </li>
-          <li>
-            <input
-              id="filter-tv"
-              type="checkbox"
-              onChange={this.props.handleChange}
-              value="tv"
-            />
-            <label htmlFor="filter-tv">TV</label>
-          </li>
-          <li>
-            <input
-              id="filter-mobile"
-              type="checkbox"
-              onChange={this.props.handleChange}
-              value="mobile"
-            />
-            <label htmlFor="filter-mobile">Mobile</label>
-          </li>
-        </ul>
-      </div>
+      <ul className="deal-filter-menu">
+        <li>
+          <input
+            id="filter-broadband"
+            type="checkbox"
+            onChange={this.props.handleChange}
+            value="broadband"
+          />
+          <label htmlFor="filter-broadband">Broadband</label>
+        </li>
+        <li>
+          <input
+            id="filter-tv"
+            type="checkbox"
+            onChange={this.props.handleChange}
+            value="tv"
+          />
+          <label htmlFor="filter-tv">TV</label>
+        </li>
+        <li>
+          <input
+            id="filter-mobile"
+            type="checkbox"
+            onChange={this.props.handleChange}
+            value="mobile"
+          />
+          <label htmlFor="filter-mobile">Mobile</label>
+        </li>
+      </ul>
     );
   }
 }
