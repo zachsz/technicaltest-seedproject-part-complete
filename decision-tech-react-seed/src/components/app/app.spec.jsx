a11111a17333b3618815a5831c74b85dba4f753a
Mathew Hawley <Mathew.Hawley@decision.tech>
2018-03-19 09:09:42 +0000

    update test to use click handler

--- decision-tech-react-seed/src/components/app/app.spec.jsx
+++ decision-tech-react-seed/src/components/app/app.spec.jsx
@@ -5,16 +5,12 @@ import App from './app.jsx';
 describe('App', () => {
   describe('GIVEN when filtering by "broadband"', () => {
     it('should show the 4 broadband only deals', () => {
-      const wrapper = mount(<App />);
       const expectedDealsIds = [6158, 4359, 4371, 5459];
-
-      wrapper.instance().handleFilterChange({
-        target: {
-          value: 'broadband'
-        }
-      });
-
+      const wrapper = mount(<App />);
+      const broadbandCheckbox = wrapper.find('.deal-filter-menu input[value="broadband"]');
       const tableRows = wrapper.find('.broadband-grid__deal');
+
+      broadbandCheckbox.simulate('click');
 
       expect(tableRows).toHaveLength(4);
       tableRows.forEach(row => {
