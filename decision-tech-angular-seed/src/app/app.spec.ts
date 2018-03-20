7356e7d23f506a090d678903702bb719aec01329
Mathew Hawley <Mathew.Hawley@decision.tech>
2018-03-20 09:41:04 +0000

    formatting of test file

--- decision-tech-angular-seed/src/app/app.spec.ts
+++ decision-tech-angular-seed/src/app/app.spec.ts
@@ -6,38 +6,42 @@ import {
   ResponseOptions,
   XHRBackend
 } from '@angular/http';
-import {MockBackend} from '@angular/http/testing';
+import { MockBackend } from '@angular/http/testing';
 
-import { AppComponent } from './app.component';
-import { NO_ERRORS_SCHEMA } from "@angular/core/";
-import { FilterStateService } from "app/services/filterStateService";
+import { App } from './app';
+import { NO_ERRORS_SCHEMA } from '@angular/core/';
+import { FilterStateService } from 'app/services/filterStateService';
 
-
-
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpModule],
-      declarations: [
-        AppComponent],
+describe('App', () => {
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpModule],
+        declarations: [App],
         schemas: [NO_ERRORS_SCHEMA],
-      providers: [        
-        FilterStateService,
-        { provide: XHRBackend, useClass: MockBackend },
-      ]
-    }).compileComponents();
-  }));
+        providers: [
+          FilterStateService,
+          { provide: XHRBackend, useClass: MockBackend }
+        ]
+      }).compileComponents();
+    })
+  );
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  it(
+    'should create the app',
+    async(() => {
+      const fixture = TestBed.createComponent(App);
+      const app = fixture.debugElement.componentInstance;
+      expect(app).toBeTruthy();
+    })
+  );
 
-  it(`should have as title 'start'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('start');
-  }));
-
+  it(
+    `should have as title 'start'`,
+    async(() => {
+      const fixture = TestBed.createComponent(App);
+      const app = fixture.debugElement.componentInstance;
+      expect(app.title).toEqual('start');
+    })
+  );
 });
